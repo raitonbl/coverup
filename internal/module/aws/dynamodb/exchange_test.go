9f94acae27047691c6c97e2593b31157a222a3db
RaitonBL <delfimarime@gmail.com>
2024-06-30 19:41:50 +0200

    wip: bootstrapping dynamodb support

--- internal/module/aws/dynamodb/exchange_test.go
+++ internal/module/aws/dynamodb/exchange_test.go
@@ -1,12 +1,12 @@
 package dynamodb
 
 import (
-	"fmt"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestGetItemResponse_ValueFrom(t *testing.T) {
+func TestValueFrom(t *testing.T) {
 	response := GetItemResponse{
 		sdkAttributes: map[string]types.AttributeValue{
 			"id":           &types.AttributeValueMemberS{Value: "96cd487b-b225-4074-9bfb-85b857b016db"},
@@ -23,12 +23,56 @@ func TestGetItemResponse_ValueFrom(t *testing.T) {
 				"template":    &types.AttributeValueMemberS{Value: "default"},
 				"screen_mode": &types.AttributeValueMemberS{Value: "dark"},
 				"zoom_in":     &types.AttributeValueMemberN{Value: "75"},
-			}},
+			},
+			},
 		},
 	}
-	valueOf, err := response.ValueFrom("id")
-	if err != nil {
-		t.Fatal(err)
+
+	tests := []struct {
+		location string
+		expected any
+	}{
+		{"id", "96cd487b-b225-4074-9bfb-85b857b016db"},
+		{"name", "RaitonBL"},
+		{"age", 21.0},
+		{"tags[0]", "Platform=Golang"},
+		{"tags[1]", "Project=Coverup"},
+		{"luck_numbers[0]", 7.0},
+		{"luck_numbers[1]", 11.0},
+		{"is_developer", true},
+		{"cli_opts[0]", "--alertsPerDay"},
+		{"cli_opts[1]", 1.0},
+		{"console_opts.template", "default"},
+		{"console_opts.screen_mode", "dark"},
+		{"console_opts.zoom_in", 75.0},
 	}
-	fmt.Println(valueOf)
+
+	for _, tt := range tests {
+		t.Run(tt.location, func(t *testing.T) {
+			value, err := response.ValueFrom(tt.location)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, value)
+		})
+	}
+}
+
+func TestInvalidLocation(t *testing.T) {
+	response := GetItemResponse{
+		sdkAttributes: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: "96cd487b-b225-4074-9bfb-85b857b016db"},
+		},
+	}
+	tests := []struct {
+		location string
+	}{
+		{"unknown"},
+		{"tags[2]"},
+		{"console_opts.invalid_key"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.location, func(t *testing.T) {
+			_, err := response.ValueFrom(tt.location)
+			assert.Error(t, err)
+		})
+	}
 }
